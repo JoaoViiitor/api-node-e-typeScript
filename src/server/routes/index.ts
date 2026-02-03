b6d88e894bb5f15ff43f3a026adb14c035dbaafa
JoaoViiitor <jv324779@gmail.com>
2026-02-03 13:53:10 -0300

    Feat: initial routes setup

---
+++ src/server/routes/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+import {StatusCodes} from 'http-status-codes';
+
+const router = Router();
+
+router.get('/', (req, res) => {
+    console.log(req);
+
+    return res.status(StatusCodes.ACCEPTED).json(req.body);
+});
+
+export { router };
